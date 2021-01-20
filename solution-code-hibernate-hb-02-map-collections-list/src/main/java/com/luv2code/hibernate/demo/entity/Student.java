7f2f5234f688e110b970ec97dfa7f63ffb32a9ee
padamv <padamv91@gmail.com>
2021-01-20 11:19:13 +0100

    Add order to images

--- solution-code-hibernate-hb-02-map-collections-list/src/main/java/com/luv2code/hibernate/demo/entity/Student.java
+++ solution-code-hibernate-hb-02-map-collections-list/src/main/java/com/luv2code/hibernate/demo/entity/Student.java
@@ -2,7 +2,6 @@ package com.luv2code.hibernate.demo.entity;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
@@ -11,7 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
+import javax.persistence.OrderColumn;
 import javax.persistence.Table;
 
 @Entity
@@ -33,6 +32,7 @@ public class Student {
 	
 	@ElementCollection
 	@CollectionTable(name="image")
+	@OrderColumn
 	@Column(name="file_name") // defaults to images
 	private List<String> images = new ArrayList<String>();
 	
