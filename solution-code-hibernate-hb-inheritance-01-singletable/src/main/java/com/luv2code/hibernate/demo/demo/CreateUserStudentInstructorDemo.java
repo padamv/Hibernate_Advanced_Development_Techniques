c35e108c1e21e62c0ad01d7bb7d9ab641acbefc8
padamv <padamv91@gmail.com>
2021-01-20 15:40:40 +0100

    Create student and instructor app

--- solution-code-hibernate-hb-inheritance-01-singletable/src/main/java/com/luv2code/hibernate/demo/demo/CreateUserStudentInstructorDemo.java
+++ solution-code-hibernate-hb-inheritance-01-singletable/src/main/java/com/luv2code/hibernate/demo/demo/CreateUserStudentInstructorDemo.java
@@ -4,17 +4,18 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import com.luv2code.hibernate.demo.entity.Status;
-import com.luv2code.hibernate.demo.entity.User;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.Student;
 
-public class CreateStudentDemo {
+public class CreateUserStudentInstructorDemo {
 
 	public static void main(String[] args) {
 		
 		// create session factory
 		SessionFactory factory = new Configuration()
 									.configure("hibernate.cfg.xml")
-									.addAnnotatedClass(User.class)
+									.addAnnotatedClass(Student.class)
+									.addAnnotatedClass(Instructor.class)
 									.buildSessionFactory();
 		
 		// create session
@@ -22,16 +23,16 @@ public class CreateStudentDemo {
 		
 		try {
 			// create the object
-			User tempStudent1 = new User ("Paul", "Wall", "paul@luv2code.com", Status.ACTIVE);
-			User tempStudent2 = new User ("Mary", "Public", "Mary@luv2code.com", Status.INACTIVE);
+			Student tempStudent = new Student ("Paul", "Wall", "paul@luv2code.com","Hibernate");
+			Instructor tempInstructor = new Instructor ("Mary", "Public", "Mary@luv2code.com", 20000.00 );
 			
 			// start a transaction
 			session.beginTransaction();
 			
 			// save the object
-			System.out.println("Saving the students...");
-			session.save(tempStudent1);
-			session.save(tempStudent2);
+			System.out.println("Saving the student and instructor...");
+			session.save(tempStudent);
+			session.save(tempInstructor);
 			
 			// commit the transaction
 			session.getTransaction().commit();
