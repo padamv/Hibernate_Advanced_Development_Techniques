a89113b7d11f988a1d2ca585d2152a7266bf5940
padamv <padamv91@gmail.com>
2021-01-20 15:43:00 +0100

    Change discriminator type

--- solution-code-hibernate-hb-inheritance-01-singletable/src/main/java/com/luv2code/hibernate/demo/entity/User.java
+++ solution-code-hibernate-hb-inheritance-01-singletable/src/main/java/com/luv2code/hibernate/demo/entity/User.java
@@ -1,6 +1,8 @@
 package com.luv2code.hibernate.demo.entity;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +14,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "user")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="USER_TYPE", discriminatorType = DiscriminatorType.STRING)
 public abstract class User {
 
 	@Id
