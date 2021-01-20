6142ce2ea900327ea37a60b3095e9ad1841c0510
padamv <padamv91@gmail.com>
2021-01-20 15:59:19 +0100

    Create from inheritance-01

---
+++ solution-code-hibernate-hb-inheritance-02-tableperclass/src/main/java/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -0,0 +1,28 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue(value = "INSTRUCTOR")
+public class Instructor extends User {
+	
+	private Double salary;
+	
+
+	public Instructor(String firstName, String lastName, String email, double salary) {
+		super(firstName, lastName, email);
+		this.salary = salary;
+	}
+
+
+	public Double getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+}
