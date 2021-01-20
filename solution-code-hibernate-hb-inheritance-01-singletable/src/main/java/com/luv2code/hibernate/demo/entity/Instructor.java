36b83ad4d5c37ff1b9f56d4a392bec24cd9752a5
padamv <padamv91@gmail.com>
2021-01-20 15:32:50 +0100

    Create instructor

---
+++ solution-code-hibernate-hb-inheritance-01-singletable/src/main/java/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -0,0 +1,26 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Instructor extends User {
+	
+	private Double salary;
+	
+
+	public Instructor(String firstName, String lastName, String email, double salary) {
+		super(firstName, lastName, email);
+		this.salary = salary;
+	}
+
+
+	public Double getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+}
