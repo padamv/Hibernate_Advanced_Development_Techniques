494272b2ecf6bcc860ca134ec61c6e073e062d9c
padamv <padamv91@gmail.com>
2021-01-20 15:44:32 +0100

    Add custom discriminator values to instructor and student

--- solution-code-hibernate-hb-inheritance-01-singletable/src/main/java/com/luv2code/hibernate/demo/entity/Instructor.java
+++ solution-code-hibernate-hb-inheritance-01-singletable/src/main/java/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -1,8 +1,10 @@
 package com.luv2code.hibernate.demo.entity;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue(value = "INSTRUCTOR")
 public class Instructor extends User {
 	
 	private Double salary;
